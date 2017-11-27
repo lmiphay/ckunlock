4a6a1649a4238cb407a6714d6fc7db15cae5a852
Paul Healy <lmiphay@gmail.com>
2017-11-27 21:50:13 +0000

    seperate task out

--- ckunlock/dbunlock.py
+++ ckunlock/dbunlock.py
@@ -5,9 +5,6 @@ from __future__ import print_function
 import subprocess
 import sys
 
-import invoke
-from invoke import task
-
 import cksession
 
 DBUS_COMMAND = 'dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit"'
@@ -16,11 +13,6 @@ UNLOCK_ARG = 'org.freedesktop.ConsoleKit.Session.Unlock'
 def unlock_cmd():
     session = '/org/freedesktop/ConsoleKit/{}'.format(cksession.CkSession().list_sessions().x11_session())
     return '{cmd} {session} {unlock_arg}'.format(cmd=DBUS_COMMAND, session=session, unlock_arg=UNLOCK_ARG)
-
-@task
-def unlock(ctx):
-    session = '/org/freedesktop/ConsoleKit/{}'.format(CkSession().list_sessions().x11_session())
-    ctx.sudo(unlock_cmd(), echo=True)
 
 if __name__ == "__main__":
     sys.exit(subprocess.call('sudo {}'.format(unlock_cmd()), shell=True))
